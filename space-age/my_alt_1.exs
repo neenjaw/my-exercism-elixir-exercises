ba627a1459a2d1940fdcebbf016ef0cec3f434d2
Tim Austin <tim@neenjaw.com>
2019-01-14 16:31:51 -0600

    solution 2

---
+++ space-age/my_alt_1.exs
@@ -0,0 +1,30 @@
+defmodule SpaceAge do
+  @type planet ::
+          :mercury
+          | :venus
+          | :earth
+          | :mars
+          | :jupiter
+          | :saturn
+          | :uranus
+          | :neptune
+
+  @doc """
+  Return the number of years a person that has lived for 'seconds' seconds is
+  aged on 'planet'.
+  """
+  @spec age_on(planet, pos_integer) :: float
+  def age_on(planet, seconds) do
+    case planet do
+      # planet -> seconds / mm / hh / dd / y
+      :mercury -> seconds / 60 / 60 / 24 / 87.969
+      :venus   -> seconds / 60 / 60 / 24 / 224.701
+      :earth   -> seconds / 60 / 60 / 24 / 365.25
+      :mars    -> seconds / 60 / 60 / 24 / 686.971
+      :jupiter -> seconds / 60 / 60 / 24 / 4332.59
+      :saturn  -> seconds / 60 / 60 / 24 / 10759.22
+      :uranus  -> seconds / 60 / 60 / 24 / 30688.5
+      :neptune -> seconds / 60 / 60 / 24 / 60182
+    end
+  end
+end
--- space-age/space_age.exs
+++ space-age/space_age.exs
@@ -15,17 +15,16 @@ defmodule SpaceAge do
   """
   @spec age_on(planet, pos_integer) :: float
   def age_on(planet, seconds) do
-    case planet do
-      # planet -> seconds / mm / hh / dd / y
-      :mercury -> seconds / 60 / 60 / 24 / 87.969
-      :venus   -> seconds / 60 / 60 / 24 / 224.701
-      :earth   -> seconds / 60 / 60 / 24 / 365.25
-      :mars    -> seconds / 60 / 60 / 24 / 686.971
-      :jupiter -> seconds / 60 / 60 / 24 / 4332.59
-      :saturn  -> seconds / 60 / 60 / 24 / 10759.22
-      :uranus  -> seconds / 60 / 60 / 24 / 30688.5
-      :neptune -> seconds / 60 / 60 / 24 / 60182
-    end
+    seconds / 60 / 60 / 24 / planet_year_days(planet)
   end
+
+  def planet_year_days(:mercury), do: 87.969
+  def planet_year_days(:venus),   do: 224.701
+  def planet_year_days(:earth),   do: 365.25
+  def planet_year_days(:mars),    do: 686.971
+  def planet_year_days(:jupiter), do: 4332.59
+  def planet_year_days(:saturn),  do: 10759.22
+  def planet_year_days(:uranus),  do: 30688.5
+  def planet_year_days(:neptune), do: 60182
 
 end
