4bd79af677eb2d9770c2c724cef451c530716a57
Tim Austin <tim@neenjaw.com>
2019-01-14 16:29:08 -0600

    solution 1

--- space-age/space_age.exs
+++ space-age/space_age.exs
@@ -15,5 +15,17 @@ defmodule SpaceAge do
   """
   @spec age_on(planet, pos_integer) :: float
   def age_on(planet, seconds) do
+    case planet do
+      # planet -> seconds / mm / hh / dd / y
+      :mercury -> seconds / 60 / 60 / 24 / 87.969
+      :venus   -> seconds / 60 / 60 / 24 / 224.701
+      :earth   -> seconds / 60 / 60 / 24 / 365.25
+      :mars    -> seconds / 60 / 60 / 24 / 686.971
+      :jupiter -> seconds / 60 / 60 / 24 / 4332.59
+      :saturn  -> seconds / 60 / 60 / 24 / 10759.22
+      :uranus  -> seconds / 60 / 60 / 24 / 30688.5
+      :neptune -> seconds / 60 / 60 / 24 / 60182
+    end
   end
+
 end
