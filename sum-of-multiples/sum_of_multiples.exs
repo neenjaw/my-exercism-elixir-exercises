75c72a4405c9306160eb8ea9915a22a533803ac8
Tim Austin <tim@neenjaw.com>
2019-01-19 12:23:09 -0600

    sum done, pangram start

--- sum-of-multiples/sum_of_multiples.exs
+++ sum-of-multiples/sum_of_multiples.exs
@@ -3,6 +3,11 @@ defmodule SumOfMultiples do
   Adds up all numbers from 1 to a given end number that are multiples of the factors provided.
   """
   @spec to(non_neg_integer, [non_neg_integer]) :: non_neg_integer
-  def to(limit, factors) do
+  def to(limit, factors) when limit >= 1 do
+    Range.new(1, limit-1)
+    |> Enum.filter(fn i ->
+      Enum.any?(factors, fn j -> rem(i, j) == 0 end)    
+    end)
+    |> Enum.reduce(0, fn i, acc -> i + acc end)
   end
 end
