d7cbdee6f71eadad47aebf11fb827ef77435cce8
Tim Austin <tim@neenjaw.com>
2019-01-26 23:23:36 -0600

    ?binary search solution

--- binary-search/binary_search.exs
+++ binary-search/binary_search.exs
@@ -19,5 +19,25 @@ defmodule BinarySearch do
 
   @spec search(tuple, integer) :: {:ok, integer} | :not_found
   def search(numbers, key) do
+    search(numbers, 0, (tuple_size(numbers)-1), key)
   end
+  
+  def search({},  _, _, _), do: :not_found
+  def search({e}, _, _, e), do: {:ok, e}
+  def search({_}, _, _, _), do: :not_found
+  
+  def search(t, first_index, last_index, goal) when first_index <= last_index do
+    midpoint = (div (last_index - first_index), 2) + first_index
+    e = elem t, midpoint
+    
+    # IO.inspect(t)
+    # IO.puts("elem: #{e}, first: #{first_index}, last: #{last_index}, midpoint: #{midpoint}, goal: #{goal}\n")
+
+    cond do
+      goal == e -> {:ok, e}
+      goal < e  -> search(t, first_index, midpoint-1, goal)
+      goal > e  -> search(t, midpoint+1, last_index, goal)
+    end
+  end
+  def search(_t, first_index, last_index, _goal) when first_index > last_index, do: :not_found
 end
