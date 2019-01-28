ec408fbff4c0719b8cdb4ab3233d8ede51a3d171
Tim Austin <tim@neenjaw.com>
2019-01-27 20:34:22 -0600

    leap solution

--- leap/leap.exs
+++ leap/leap.exs
@@ -9,6 +9,8 @@ defmodule Year do
       unless the year is also evenly divisible by 400
   """
   @spec leap_year?(non_neg_integer) :: boolean
-  def leap_year?(year) do
-  end
+  def leap_year?(year) when rem(year, 400) == 0, do: true
+  def leap_year?(year) when rem(year, 100) == 0, do: false
+  def leap_year?(year) when rem(year, 4) == 0, do: true
+  def leap_year?(year), do: false
 end
