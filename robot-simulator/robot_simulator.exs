b67b0ab956aef9858f5ae1077c29abdb1d04e0e5
Tim Austin <tim@neenjaw.com>
2019-01-29 15:25:56 -0600

    robot solution

--- robot-simulator/robot_simulator.exs
+++ robot-simulator/robot_simulator.exs
@@ -1,21 +1,75 @@
 defmodule RobotSimulator do
+
+  defmodule Robot do
+    @enforce_keys [:direction, :x_position, :y_position]
+    defstruct direction: nil,
+              x_position: nil, y_position: nil
+
+    @type t() :: %__MODULE__{
+      direction: atom(),
+      x_position: integer(),
+      y_position: integer(),
+    }
+  end
+
+  @valid_directions [:north, :east, :south, :west]
+
   @doc """
   Create a Robot Simulator given an initial direction and position.
 
   Valid directions are: `:north`, `:east`, `:south`, `:west`
   """
-  @spec create(direction :: atom, position :: {integer, integer}) :: any
-  def create(direction \\ nil, position \\ nil) do
-  end
+  @spec create(direction :: atom, position :: {integer, integer}) :: Robot
+  def create(direction \\ :north, position \\ {0,0})
+  def create(direction, {x, y}) 
+    when direction in @valid_directions and is_integer(x) and is_integer(y), 
+    do: %Robot{direction: direction, x_position: x, y_position: y}
+  # Match invalid position, return error
+  def create(direction, _) when direction in @valid_directions, do: {:error, "invalid position"}
+  # Match invalid direction, return error
+  def create(_, _), do: {:error, "invalid direction"}
 
   @doc """
   Simulate the robot's movement given a string of instructions.
 
   Valid instructions are: "R" (turn right), "L", (turn left), and "A" (advance)
   """
-  @spec simulate(robot :: any, instructions :: String.t()) :: any
+  @spec simulate(robot :: Robot, instructions :: String.t()) :: Robot
   def simulate(robot, instructions) do
+    instructions
+    |> String.upcase
+    |> String.graphemes
+    |> Enum.reduce(robot, fn 
+      # Match to previous error and pass it on
+      _, {:error, msg} -> {:error, msg}
+      # Match to valid instructions
+      "L", r -> turn_robot(r, "L")
+      "R", r -> turn_robot(r, "R")
+      "A", r -> advance_robot(r)
+      # Match to invalid instruction, return error
+      _, _   -> {:error, "invalid instruction"}
+    end)
   end
+
+
+  defp turn_robot(robot, direction) do
+    %{robot | direction: turn(direction, robot.direction)}
+  end
+
+  defp turn("L", :north), do: :west
+  defp turn("L", :west),  do: :south
+  defp turn("L", :south), do: :east
+  defp turn("L", :east),  do: :north
+
+  defp turn("R", :north), do: :east
+  defp turn("R", :east),  do: :south
+  defp turn("R", :south), do: :west
+  defp turn("R", :west),  do: :north
+
+  defp advance_robot(%Robot{direction: :north} = robot), do: %{robot | y_position: (robot.y_position+1)}
+  defp advance_robot(%Robot{direction: :south} = robot), do: %{robot | y_position: (robot.y_position-1)}
+  defp advance_robot(%Robot{direction: :east}  = robot), do: %{robot | x_position: (robot.x_position+1)}
+  defp advance_robot(%Robot{direction: :west}  = robot), do: %{robot | x_position: (robot.x_position-1)}
 
   @doc """
   Return the robot's direction.
@@ -23,13 +77,11 @@ defmodule RobotSimulator do
   Valid directions are: `:north`, `:east`, `:south`, `:west`
   """
   @spec direction(robot :: any) :: atom
-  def direction(robot) do
-  end
+  def direction(%Robot{direction: d}), do: d
 
   @doc """
   Return the robot's position.
   """
-  @spec position(robot :: any) :: {integer, integer}
-  def position(robot) do
-  end
+  @spec position(robot :: Robot) :: {integer, integer}
+  def position(%Robot{x_position: x, y_position: y}), do: {x,y}
 end
