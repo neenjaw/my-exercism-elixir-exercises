c7e9523de409b8e93f21f922108171802089c987
Tim Austin <tim@neenjaw.com>
2019-04-07 15:38:31 -0600

    perfect numbers soln

--- perfect-numbers/perfect_numbers.exs
+++ perfect-numbers/perfect_numbers.exs
@@ -1,4 +1,7 @@
 defmodule PerfectNumbers do
+
+  defguard is_natural_number(number) when is_integer(number) and number > 0
+
   @doc """
   Determine the aliquot sum of the given `number`, by summing all the factors
   of `number`, aside from `number` itself.
@@ -10,6 +13,28 @@ defmodule PerfectNumbers do
   :deficient if the aliquot sum is less than `number`
   """
   @spec classify(number :: integer) :: {:ok, atom} | {:error, String.t()}
-  def classify(number) do
+
+  def classify(number) when is_natural_number(number) do
+    classification =
+      case do_aliquot_sum(number) do
+        sum when sum == number -> :perfect
+        sum when sum >  number -> :abundant
+        sum when sum <  number -> :deficient
+      end
+
+    {:ok, classification}
+  end
+  def classify(_) do
+    {:error, "Classification is only possible for natural numbers."}
+  end
+
+  defp do_aliquot_sum(target, current \\ 2, factors \\ [1])
+  defp do_aliquot_sum(1, _, _), do: 0
+  defp do_aliquot_sum(target, target, factors), do: Enum.sum(factors)
+  defp do_aliquot_sum(target, current, factors) when rem(target, current) == 0 do
+    do_aliquot_sum(target, current + 1, [current | factors])
+  end
+  defp do_aliquot_sum(target, current, factors) do
+    do_aliquot_sum(target, current + 1, factors)
   end
 end
