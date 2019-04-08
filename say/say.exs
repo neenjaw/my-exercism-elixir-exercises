784ddb2de385fb0e669bfad2286fcd0337a58485
Tim Austin <tim@neenjaw.com>
2019-04-08 10:26:34 -0600

    submitted solutions

--- say/say.exs
+++ say/say.exs
@@ -1,4 +1,3 @@
-
 defmodule Say do
 
   @one_trillion 1_000_000_000_000
@@ -13,39 +12,43 @@ defmodule Say do
     when n > 0 
     and n < @one_trillion
   
-
   @doc """
   Translate a positive integer into English.
   """
   @spec in_english(integer) :: {atom, String.t()}
   def in_english(number) when in_range(number) do
-    number
-    |> Integer.digits
-    |> Enum.reverse
-    |> Enum.chunk_every(3, 3, [0,0])
-    |> Enum.map(&say_chunk/1)
-    |> Enum.zip(@chunk_names)
-    |> Enum.filter(fn
-         {"", _} -> false
-         _chunk  -> true
-       end)
-    |> Enum.map(fn 
-         {chunk, name} -> "#{chunk} #{name}" 
-       end)
-    |> Enum.reverse
-    |> Enum.join("\s")
+    english = 
+      number
+      |> Integer.digits
+      |> Enum.reverse
+      |> Enum.chunk_every(3, 3, [0,0])
+      |> Enum.map(&convert_chunk_to_english/1)
+      |> Enum.zip(@chunk_names)
+      |> Enum.filter(fn
+          {"", _} -> false
+          _chunk  -> true
+        end)
+      |> Enum.map(fn 
+          {chunk, ""} -> chunk 
+          {chunk, name} -> "#{chunk} #{name}" 
+        end)
+      |> Enum.reverse
+      |> Enum.join("\s")
+
+    {:ok, english}
   end
   
-  def in_english(0), do: "zero"
+  def in_english(0), do: {:ok, "zero"}
+  def in_english(_), do: {:error, "number is out of range"}
   
-  def say_chunk([one, ten, hundred]) do
-    hundred_place_name = 
+  defp convert_chunk_to_english([one, ten, hundred]) do
+    hundred_place = 
       case hundred do
         0 -> :none
-        _ -> "#{number_name(hundred)} hundred"
+        _ -> "#{number_in_english(hundred)} hundred"
       end
       
-    [hundred_place_name, ten_place_name(ten, one)]
+    [hundred_place, convert_ten_place_to_english(ten, one)]
     |> Enum.filter(fn 
          :none -> false
          _name -> true
@@ -53,7 +56,7 @@ defmodule Say do
     |> Enum.join("\s")
   end
   
-  def number_name(n) do
+  defp number_in_english(n) do
     case n do
       0 -> :none
       1 -> "one"
@@ -68,9 +71,9 @@ defmodule Say do
     end
   end
   
-  def ten_place_name(ten, one) do
+  defp convert_ten_place_to_english(ten, one) do
     case {ten, one} do
-      {0, _} -> number_name(one)
+      {0, _} -> number_in_english(one)
         
       {1, 0} -> "ten"
       {1, 1} -> "eleven"
@@ -84,28 +87,28 @@ defmodule Say do
       {1, 9} -> "nineteen"
         
       {2, 0} -> "twenty"
-      {2, _} -> "twenty-#{number_name(one)}"
+      {2, _} -> "twenty-#{number_in_english(one)}"
         
       {3, 0} -> "thirty"
-      {3, _} -> "thirty-#{number_name(one)}"
+      {3, _} -> "thirty-#{number_in_english(one)}"
         
       {4, 0} -> "forty"
-      {4, _} -> "forty-#{number_name(one)}"
+      {4, _} -> "forty-#{number_in_english(one)}"
         
       {5, 0} -> "fifty"
-      {5, _} -> "fifty-#{number_name(one)}"
+      {5, _} -> "fifty-#{number_in_english(one)}"
         
       {6, 0} -> "sixty"
-      {6, _} -> "sixty-#{number_name(one)}"
+      {6, _} -> "sixty-#{number_in_english(one)}"
         
       {7, 0} -> "seventy"
-      {7, _} -> "seventy-#{number_name(one)}"
+      {7, _} -> "seventy-#{number_in_english(one)}"
         
       {8, 0} -> "eighty"
-      {8, _} -> "eighty-#{number_name(one)}"
+      {8, _} -> "eighty-#{number_in_english(one)}"
         
       {9, 0} -> "ninety"
-      {9, _} -> "ninety-#{number_name(one)}"
+      {9, _} -> "ninety-#{number_in_english(one)}"
     end
   end
 end
--- simple-cipher/simple_cipher.exs
+++ simple-cipher/simple_cipher.exs
@@ -1,102 +1,76 @@
-
-
-
 defmodule SimpleCipher do
 
-  @start ?a
-  @end ?z
+  @range_start ?a
+  @range_end ?z
 
-  defguardp is_encodable(c) when c in @start..@end
+  defguardp is_encodable(c) when c in @range_start..@range_end
 
   @doc """
   Given a `plaintext` and `key`, encode each character of the `plaintext` by
   shifting it by the corresponding letter in the alphabet shifted by the number
   of letters represented by the `key` character, repeating the `key` if it is
   shorter than the `plaintext`.
-
-  For example, for the letter 'd', the alphabet is rotated to become:
-
-  defghijklmnopqrstuvwxyzabc
-
-  You would encode the `plaintext` by taking the current letter and mapping it
-  to the letter in the same position in this rotated alphabet.
-
-  abcdefghijklmnopqrstuvwxyz
-  defghijklmnopqrstuvwxyzabc
-
-  "a" becomes "d", "t" becomes "w", etc...
-
-  Each letter in the `plaintext` will be encoded with the alphabet of the `key`
-  character in the same position. If the `key` is shorter than the `plaintext`,
-  repeat the `key`.
-
-  Example:
-
-  plaintext = "testing"
-  key = "abc"
-
-  The key should repeat to become the same length as the text, becoming
-  "abcabca". If the key is longer than the text, only use as many letters of it
-  as are necessary.
   """
   def encode(plaintext, key) do
-    cipher_stream = 
+    cipher_stream =
       create_cipher_stream(key)
 
-    plaintext 
-    |> String.codepoints
-    |> Stream.transform(cipher_stream, &cipher_transform(&1, &2, &encode_codepoint/2))
+    plaintext
+    |> to_charlist
+    |> Stream.concat([:end])
+    |> Stream.transform(cipher_stream, fn c, c_stream ->
+         cipher_transform(c, c_stream, &encode_codepoint/2)
+       end)
     |> Enum.to_list
     |> to_string
   end
-  
-  def create_cipher_stream(key) do
+
+  defp create_cipher_stream(key) do
     key
-    |> String.codepoints
+    |> to_charlist
     |> Stream.filter(fn
-       c when c in ?a..?z -> true
-         _ -> false
-       end)
-    |> Stream.map(&(?a - &1))
+        c when c in @range_start..@range_end -> true
+        _ -> false
+      end)
+    |> Stream.map(&(&1 - @range_start))
     |> Stream.cycle
   end
-  
-  def cipher_transform(c, c_stream, t_fun) 
-    when is_encodeable(c) do
 
+  defp cipher_transform(c, c_stream, transform_fx) when is_encodable(c) do
     shift =
-      c_stream 
-      |> Stream.take(1) 
-      |> Enum.to_list 
+      c_stream
+      |> Stream.take(1)
+      |> Enum.to_list
       |> List.first
-             
+
     next_c_stream =
       Stream.drop(c_stream, 1)
-         
-    {
-      [t_fun.(c, shift)],
-      next_c_stream
-    }
+
+    {[transform_fx.(c, shift)], next_c_stream}
   end
-           
-  def cipher_transform(c, c_stream) do
+
+  defp cipher_transform(:end, c_stream, _) do
+    {:halt, c_stream}
+  end
+
+  defp cipher_transform(c, c_stream, _) do
     {[c], c_stream}
   end
-  
-  def encode_codepoint(codepoint, shift) do
-    rem((codepoint - @start + shift), 26) + @start
+
+  defp encode_codepoint(codepoint, shift) do
+    rem((codepoint - @range_start + shift), 26) + @range_start
   end
- 
-  def decode_codepoint(codepoint, shift) do
-    case rem((codepoint - @start - shift), 26) do
-      c when c < 0 -> 
-        c + 26 + @start
-        
+
+  defp decode_codepoint(codepoint, shift) do
+    case rem((codepoint - @range_start - shift), 26) do
+      c when c < 0 ->
+        c + 26 + @range_start
+
       c ->
-        c + @start
+        c + @range_start
     end
   end
-  
+
 
   @doc """
   Given a `ciphertext` and `key`, decode each character of the `ciphertext` by
@@ -109,13 +83,15 @@ defmodule SimpleCipher do
   etc..., depending on how much you shift the alphabet.
   """
   def decode(ciphertext, key) do
-  
-  cipher_stream = 
-    create_cipher_stream(key)
+    cipher_stream =
+      create_cipher_stream(key)
 
-    plaintext 
-    |> String.codepoints
-    |> Stream.transform(cipher_stream, &cipher_transform(&1, &2, &decode_codepoint/2))
+    ciphertext
+    |> to_charlist
+    |> Stream.concat([:end])
+    |> Stream.transform(cipher_stream, fn c, c_stream ->
+         cipher_transform(c, c_stream, &decode_codepoint/2)
+       end)
     |> Enum.to_list
     |> to_string
   end
