debcf396f3aa373464555c133b56396f05d68600
Tim Austin <tim@neenjaw.com>
2019-04-15 18:16:41 -0600

    main done, but no tie braking of equal type hands

--- poker/poker.exs
+++ poker/poker.exs
@@ -33,10 +33,21 @@ defmodule Poker do
       suit_map: %{club: 0, spade: 0, heart: 0, diamond: 0},
       value_map: 1..14 |> Enum.map(fn v -> {v, 0} end) |> Map.new,
       card_list: [],
-      card_count: 0
+      card_count: 0,
+      attributes: []
     ]
+    def new(card_string_list) do
+      card_string_list
+      |> Enum.reverse()
+      |> add_cards()
+    end
 
-    def add_card(hand \\ %Poker.Hand{}, card_string) do
+    defp add_cards(hand \\ %Poker.Hand{}, card_string_list) do
+      card_string_list
+      |> Enum.reduce(hand, fn c, hand -> add_card(hand, c) end)
+    end
+
+    defp add_card(hand \\ %Poker.Hand{}, card_string) do
       {value, suit} = String.split_at(card_string, -1)
 
       hand
@@ -46,17 +57,12 @@ defmodule Poker do
       |> increment_card_count()
     end
 
-    def add_cards(hand \\ %Poker.Hand{}, card_string_list) do
-      card_string_list
-      |> Enum.reduce(hand, fn c, hand -> add_card(hand, c) end)
-    end
-
     @doc """
     Add the value to the hand struct
     """
     defp add_card_value(hand, "A") do
       hand = update_in(hand.value_map[1], &(&1 + 1))
-      hand = update_in(hand.value_map[14], &(&1 + 1))
+      # hand = update_in(hand.value_map[14], &(&1 + 1))
     end
     defp add_card_value(hand, value) when value in ~w(2 3 4 5 6 7 8 9 10) do
       n = String.to_integer(value)
@@ -82,21 +88,148 @@ defmodule Poker do
     defp increment_card_count(hand, amount \\ 1), do: update_in(hand.card_count, &(&1 + amount))
   end
 
+  defguardp is_sequence(a,b,c,d,e)
+    when (a == 1 and b == 10 and c == 11 and d == 12 and e == 13)
+    or (b == a+1 and c == b+1 and d == c+1 and e ==d+1)
+
   @doc """
   Score and rank the hands, returning the highest hand
   """
   @spec best_hand(list(list(String.t()))) :: list(list(String.t()))
   def best_hand(hands) do
-    analyse_hands(hands)
+    highest_hand =
+      hands
+      |> analyse_hands()
+      |> compare_hands()
+
+    [highest_hand.card_list]
   end
 
   def analyse_hands(hands, results \\ [])
+  def analyse_hands([], results), do: results
   def analyse_hands([hand | hands], results) do
+    result =
+      hand
+      |> Poker.Hand.new()
+      |> add_attributes()
 
-
-
-
-    analyse_hands(hands, [nil | results])
+    analyse_hands(hands, [result | results])
   end
 
+  defp add_attributes(hand = %Poker.Hand{}) do
+    hand
+    |> add_flush_attr?()
+    |> add_x_of_a_kind_attr?()
+    |> add_straight_attr?()
+  end
+
+  defp add_flush_attr?(hand = %Poker.Hand{}) do
+    hand.suit_map
+    |> Map.to_list()
+    |> Enum.filter(fn {suit, count} -> if count == 5, do: true, else: false end)
+    |> (fn
+      [] -> hand
+      [{_suit, 5}] -> update_in(hand.attributes, &([:flush | &1]))
+    end).()
+  end
+
+  defp add_x_of_a_kind_attr?(hand = %Poker.Hand{}) do
+    hand.value_map
+    |> Map.to_list()
+    |> Enum.sort(fn
+      {_v1, x}, {_v2, y} when x >= y -> true
+      _, _ -> false
+    end)
+    |> Enum.take_while(fn {_v, c} -> c > 0 end)
+    |> case do
+      [{_, 4}, {_, 1}] -> :four_of_a_kind
+      [{_, 3}, {_, 2}] -> :full_house
+      [{_, 3}, {_, 1}, {_, 1}] -> :three_of_a_kind
+      [{_, 2}, {_, 2}, {_, 1}] -> :two_pair
+      [{_, 2}, {_, 1}, {_, 1}, {_, 1}] -> :one_pair
+      [{_, 1}, {_, 1}, {_, 1}, {_, 1}, {_, 1}] -> :high_card
+      _ -> raise ArgumentError
+    end
+    |> case do
+      attr -> update_in(hand.attributes, &([attr | &1]))
+    end
+  end
+
+  defp add_straight_attr?(hand = %Poker.Hand{}) do
+    hand.value_map
+    |> Map.to_list()
+    |> Enum.filter(fn
+      {_, 1} -> true
+      _      -> false
+    end)
+    |> case do
+      list when length(list) != 5 -> hand
+
+      list ->
+        list
+        |> Enum.map(fn {v, _c} -> v end)
+        |> Enum.sort()
+        |> case do
+          [a,b,c,d,e] when not is_sequence(a,b,c,d,e) -> hand
+
+          _ -> update_in(hand.attributes, &([:straight | &1]))
+        end
+    end
+  end
+
+  @order_of_hands [
+    [:straight, :flush],
+    :four_of_a_kind,
+    :full_house,
+    :flush,
+    :straight,
+    :three_of_a_kind,
+    :two_pair,
+    :one_pair,
+    :high_card
+  ]
+
+  defp compare_hands(hands, best_hand \\ nil)
+  defp compare_hands([], best_hand), do: best_hand
+  defp compare_hands([hand | hands], nil), do: compare_hands(hands, hand)
+  defp compare_hands([hand | hands], best_hand) do
+    combined_attributes =
+      hand.attributes ++ best_hand.attributes
+
+    highest_category =
+      @order_of_hands
+      |> Enum.find(fn
+        category when is_list(category) ->
+          category
+          |> Enum.all?(fn c ->
+            (c in hand.attributes) or (c in best_hand.attributes)
+          end)
+
+        category ->
+          (category in hand.attributes) or (category in best_hand.attributes)
+      end)
+
+    new_best_hand =
+      [hand, best_hand]
+      |> Enum.filter(fn hand ->
+        case highest_category do
+          category when is_list(category) ->
+            category
+            |> Enum.all?(fn c -> c in hand.attributes end)
+
+          category ->
+            category in hand.attributes
+        end
+      end)
+      |> (fn
+        hands when length(hands) == 1 -> List.first(hands)
+        hands when length(hands) == 2 -> break_tie(hands, highest_category)
+      end).()
+
+    compare_hands(hands, new_best_hand)
+  end
+
+  def break_tie([a, b], category) do
+    IO.inspect binding()
+  end
 end
