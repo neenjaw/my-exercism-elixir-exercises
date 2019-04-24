4e44ef43b857d9088aaee3aa0b1a70dec80fe688
Tim Austin <tim@neenjaw.com>
2019-04-23 20:48:04 -0600

    pythagorean soln

--- pythagorean-triplet/pythagorean_triplet.exs
+++ pythagorean-triplet/pythagorean_triplet.exs
@@ -4,6 +4,7 @@ defmodule Triplet do
   """
   @spec sum([non_neg_integer]) :: non_neg_integer
   def sum(triplet) do
+    Enum.sum(triplet)
   end
 
   @doc """
@@ -11,6 +12,7 @@ defmodule Triplet do
   """
   @spec product([non_neg_integer]) :: non_neg_integer
   def product(triplet) do
+    Enum.reduce(triplet, fn a, b -> a * b end)
   end
 
   @doc """
@@ -18,13 +20,24 @@ defmodule Triplet do
   """
   @spec pythagorean?([non_neg_integer]) :: boolean
   def pythagorean?([a, b, c]) do
+    (a*a) + (b*b) == (c*c)
   end
 
   @doc """
   Generates a list of pythagorean triplets from a given min (or 1 if no min) to a given max.
   """
   @spec generate(non_neg_integer, non_neg_integer) :: [list(non_neg_integer)]
-  def generate(min, max) do
+  def generate(min \\ 1, max) do
+    # b is always 1 more than a so that it can't have duplicate entries like {3,4,5} and {4,3,5}
+    for a <- min..max,
+        b <- min..max,
+        c <- min..max,
+        a < b,
+        b < c,
+        pythagorean?([a,b,c])
+    do
+      [a,b,c]
+    end
   end
 
   @doc """
@@ -32,5 +45,7 @@ defmodule Triplet do
   """
   @spec generate(non_neg_integer, non_neg_integer, non_neg_integer) :: [list(non_neg_integer)]
   def generate(min, max, sum) do
+    generate(min, max)
+    |> Enum.filter(fn t -> Triplet.sum(t) == sum end)
   end
 end
