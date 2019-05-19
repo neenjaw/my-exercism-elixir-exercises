458daf2ff2d13021b38403f577969c3f15abf55c
Tim Austin <tim@neenjaw.com>
2019-05-18 22:57:22 -0600

    BST without ++ operator

--- binary-search-tree/binary_search_tree.exs
+++ binary-search-tree/binary_search_tree.exs
@@ -13,8 +13,8 @@ defmodule BinarySearchTree do
   Creates and inserts a node with its value as 'data' into the tree.
   """
   @spec insert(bst_node, any) :: bst_node
-  def insert(tree = %{data: d, left:  nil}, data) when data <= d, do: %{tree | left:  new(data)} 
-  def insert(tree = %{data: d, right: nil}, data) when data > d,  do: %{tree | right: new(data)} 
+  def insert(tree = %{data: d, left:  nil}, data) when data <= d, do: %{tree | left:  new(data)}
+  def insert(tree = %{data: d, right: nil}, data) when data > d,  do: %{tree | right: new(data)}
   def insert(tree = %{data: d, left:  l},   data) when data <= d, do: %{tree | left:  insert(l, data)}
   def insert(tree = %{data: d, right: r},   data) when data > d,  do: %{tree | right: insert(r, data)}
 
@@ -22,10 +22,12 @@ defmodule BinarySearchTree do
   Traverses the Binary Search Tree in order and returns a list of each node's data.
   """
   @spec in_order(bst_node) :: [any]
-  def in_order(tree), do: do_in_order(tree)
+  def in_order(tree), do: do_in_order(tree, [])
 
-  defp do_in_order(nil), do: []
-  defp do_in_order(%{data: d, left: l, right: r}) do
-    do_in_order(l) ++ [d] ++ do_in_order(r)
-  end    
+  defp do_in_order(nil, greater), do: greater
+  defp do_in_order(%{data: d, left: l, right: r}, prev_greater_elements) do
+    greater_elements = do_in_order(r, prev_greater_elements)
+
+    do_in_order(l, [d | greater_elements])
+  end
 end
