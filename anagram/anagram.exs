a55b30ad0074e3356278d1a636a2d97e4e2777d0
Tim Austin <tim@neenjaw.com>
2019-07-21 23:10:33 -0700

    Update anagram.exs
    Because I am better in elixir now.

--- anagram/anagram.exs
+++ anagram/anagram.exs
@@ -4,22 +4,35 @@ defmodule Anagram do
   """
   @spec match(String.t(), [String.t()]) :: [String.t()]
   def match(base, candidates) do
-    base_length = base |> String.length
-    base_sorted = base |> sort_string
-    base_downcase = base |> String.downcase
+    base_length    = String.length(base)
+    base_downcased = String.downcase(base)
+    base_sorted    = sort_string(base_downcased)
     
     candidates
-    |> Enum.filter(fn c -> base_length == String.length(c) end)
-    |> Enum.map(fn c -> {c, c |> String.downcase, c |> sort_string} end)
-    |> Enum.filter(fn {c, d, s} -> (d != base_downcase) and (s == base_sorted) end)
-    |> Enum.map(fn {c, _, _} -> c end)
+    |> Enum.filter(&filter_by_length(&1, base_length))
+    |> Enum.map(&create_candidate_tuple)
+    |> Enum.filter(&filter_if_not_anagram(&1, base_downcased, base_sorted))
+    |> Enum.map(fn {candidate, _, _} -> candidate end)
   end
 
   defp sort_string(string) do
     string
-    |> String.downcase
     |> to_charlist
     |> Enum.sort
     |> to_string
+  end
+  
+  defp filter_by_length(candidate, matching_length) do
+    matching_length == String.length(candidate)
+  end
+
+  defp create_candidate_tuple(candidate) do
+    candidate_downcased = String.downcase(candidate)
+
+    {candidate, candidate_downcased, sort_string(candidate_downcased)} 
+  end
+  
+  defp filter_if_not_anagram({_, candidate_downcased, candidate_sorted}, base_downcased, base_sorted) do
+    (candidate_downcased != base_downcased) and (candidate_sorted == base_sorted)
   end
 end
